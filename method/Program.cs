066185003ff80655cd4172aae67a6c5708dac50a
Vic Gouwy <vic.gouwy@sgsintpaulus.eu>
2022-01-31 11:10:15 +0100

    ok

---
+++ method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    class Program
+    {
+       static void verschil()
+        {
+            int   getal , get ; get = int.Parse(Console.ReadLine()); getal = int.Parse(Console.ReadLine());
+            Console.WriteLine("het verschil is {0}", getal - get);
+        }
+        static void Main(string[] args)
+        {
+            verschil();
+        }
+    }
+}
