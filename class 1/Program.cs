3fade4af9ff3efd823a682c24786f532fe33383b
Vic Gouwy <vic.gouwy@sgsintpaulus.eu>
2022-02-18 11:54:14 +0100

    ok

--- class 1/Program.cs
+++ class 1/Program.cs
@@ -10,25 +10,11 @@ namespace class_1
     {
         static void Main(string[] args)
         {
-           int getal ;
-           char teken ;
-           teken = '+' ; 
-           som.teken();
+         
         }
     }
-    static class som
-    {
-        static int optelling()
-        {
-            int totaal ;
-            totaal = 0 
-             char teken ;
-           teken = '+' ; 
-            teken = char.Parse(Console.ReadLine());
-            Console.Write("{0}", teken);
-        }
+   
 
 
-
-    }
+    
 }
---
+++ class 1/rekenmachine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	static int rekenmachine()
+	{
+		
+	}
+}
